4e8ffab09a006c638efd4cef0b47aa100520d0ed
jishnu v sarmah <jishnusarmah1398oda@gmail.com>
2021-09-23 18:07:11 +0530

    2nd commit

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app=express();
 
@@ -23,8 +24,6 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-
 const User = new mongoose.model("User", userSchema);
 
 app.get("/",(req,res)=>{
@@ -40,17 +39,21 @@ app.get("/register",(req,res)=>{
 });
 
 app.post("/register", function(req, res){
-  const newUser =  new User({
-    email: req.body.username,
-    password: req.body.password
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser =  new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
-  newUser.save(function(err){
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  });
+
 });
 
 app.post("/login", function(req, res){
@@ -62,12 +65,15 @@ app.post("/login", function(req, res){
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("secrets");
+          }
+        });
       }
     }
   });
+  
 });
 
 app.listen(3000, function() {
